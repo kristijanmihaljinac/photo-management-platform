402e57b1e93179908ec2bba58efff12f7ed2f6d1
Kristijan Mihaljinac <kristijanm@in2.hr>
2022-10-30 11:54:43 +0100

    added package

--- src/PhotoManagementPlatform.Backend/PhotoManagementPlatform.Domain/Constants/DeliveryStates.cs
+++ src/PhotoManagementPlatform.Backend/PhotoManagementPlatform.Domain/Constants/DeliveryStates.cs
@@ -1,12 +1,10 @@
-﻿using System;
-namespace PhotoManagementPlatform.Domain.Constants;
+﻿namespace PhotoManagementPlatform.Domain.Constants;
 
 public static class DeliveryStates
 {
     public const string InProgress = "InProgress";
     public const string Delivered = "Delivered";
     public const string Stopped = "Stopped";
-
 }
 
 
