f74f66ad4a8aa402b0785f5e4de6e81402feafe9
Kristijan Mihaljinac <kristijanm@in2.hr>
2022-11-03 10:54:26 +0100

    added outbox message consumer migration

--- src/PhotoManagementPlatform.Backend/PhotoManagementPlatform.Persistence/Configurations/OutboxMessageConsumerConfiguration.cs
+++ src/PhotoManagementPlatform.Backend/PhotoManagementPlatform.Persistence/Configurations/OutboxMessageConsumerConfiguration.cs
@@ -2,11 +2,6 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using PhotoManagementPlatform.Persistence.Constants;
 using PhotoManagementPlatform.Persistence.Outbox;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhotoManagementPlatform.Persistence.Configurations;
 
---
+++ src/PhotoManagementPlatform.Backend/PhotoManagementPlatform.Persistence/Migrations/20221103095225_AddedOutboxMessageConsumers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhotoManagementPlatform.Persistence.Migrations
+{
+    public partial class AddedOutboxMessageConsumers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OutboxMessageConsumers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxMessageConsumers", x => new { x.Id, x.Name });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OutboxMessageConsumers");
+        }
+    }
+}
